5b03b3d797f739b0804beeb9e1caa8907eb22046
kenweechin <kenweechin@gmail.com>
2022-05-10 22:09:40 +0000

    Updated add to cart view and add volume logic into context processor in cart app

--- cart/views.py
+++ cart/views.py
@@ -14,15 +14,32 @@ def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     # Get the redirect url from the form
     redirect_url = request.POST.get('redirect_url')
+    volume = None
+    if 'product_volume' in request.POST:
+        volume = request.POST['product_volume']
     # Get the cart variable if already exists in the session else create one 
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        # Update cart quantity if cart already exists
-        cart[item_id] += quantity
+    if volume: 
+        # Check if the item is in the cart already
+        if item_id in list(cart.keys()):
+            # Check if another item of the samd id and same volume exists already
+            if volume in cart[item_id]['items_by_volume'].keys():
+                # Then increment the quantity for that volume
+                cart[item_id]['items_by_volume'][volume] += quantity
+            else:
+                # Or else set equal to the quantity which is a new volume for the item
+                cart[item_id]['items_by_volume'][volume] = quantity
+        else: 
+            # Add the item if is not in the cart
+            cart[item_id] = {'items_by_volume': {volume: quantity}}
     else:
-        # Add item to the cart
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            # Update cart quantity if cart already exists
+            cart[item_id] += quantity
+        else:
+            # Add item to the cart
+            cart[item_id] = quantity
     
     # Override the variable in the session with the updated cart
     request.session['cart'] = cart
