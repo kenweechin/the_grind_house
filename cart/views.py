cbfdfe93c3c0d8b5f28a8dae1cdf10ff2cc01d78
kenweechin <kenweechin@gmail.com>
2022-05-16 22:06:07 +0000

    Fixed the data attribute error in the remove button link in cart page

--- cart/views.py
+++ cart/views.py
@@ -96,4 +96,5 @@ def remove_cart(request, item_id):
         request.session['cart'] = cart
         return HttpResponse(status=200)
     except Exception as e:
+        print(f"ERROR: {e}")
         return HttpResponse(status=500)
