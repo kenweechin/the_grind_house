680640e53519aa9037c20445e49a3c49b3ca77f0
kenweechin <kenweechin@gmail.com>
2022-05-23 11:50:56 +0000

    Added checkout templates and views

--- the_grind_house/settings.py
+++ the_grind_house/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+
+    # Others
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'the_grind_house.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +79,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', #required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
