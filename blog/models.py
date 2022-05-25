4fdcc1d6ad06e30cb1afa94493d5ed284d67fc3a
kenweechin <kenweechin@gmail.com>
2022-05-25 21:13:29 +0000

    Added blog link section in the home page

--- blog/models.py
+++ blog/models.py
@@ -12,8 +12,8 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE)
-    updated_on = models.DateTimeField(auto_now= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -22,12 +22,14 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created_on']
 
+
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', 
+        on_delete=models.CASCADE)
     name = models.CharField(max_length=200, unique=True)
     email = models.EmailField()
     content = models.TextField()
